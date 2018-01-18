fadb5cf46ae174d6580e7644000cdf34b8f5768d
kovsky0 <kovsky0@users.noreply.github.com>
2018-01-18 20:09:16 +0100

    Add first real test

--- server/tests/example.test.js
+++ server/tests/example.test.js
@@ -1,3 +1,21 @@
-it('testing with jest should work', () => {
-    expect(true).toBe(true)
+const request = require('supertest');
+const app = require('../app')
+const iconv = require('iconv-lite');
+
+it('should register a new user', () => {
+    const newUser = {
+        "email": "tests@test.com",
+        "password": "testtesttest"
+    }
+
+    //not entirely sure why, but without this line the api returns 415... 
+    iconv.encode(newUser, 'utf8')
+
+    return request(app)
+        .post('/api/user/signup')
+        .set('Content-Type', 'application/json')
+        .send(newUser)
+        .then(response => {
+            expect(response.body.message).toBe('User created')
+        })
 })
